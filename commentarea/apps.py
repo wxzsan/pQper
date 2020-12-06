9aea4e5927215476dbea2dccb60c28aa1c695984
王昕兆 <wxz@wxz.local>
2020-12-06 16:25:20 +0800

    增加了commentarea app 完成了model部分

---
+++ commentarea/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CommentareaConfig(AppConfig):
+    name = 'commentarea'
---
+++ commentarea/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+class Paper(models.Model):
+    title = models.CharField(max_length=255)
+    path = models.CharField(max_length=255)
+
+class ShortComment(models.Model):
+    poster = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    post_time = models.DateTimeField(auto_now_add=True)
+    content = models.CharField(max_length=255)
+    rose_number = models.IntegerField(default=0)
+    rose_user_list = models.ManyToManyField('user.User')
+
+class LongComment(models.Model):
+    poster = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    post_time = models.DateTimeField(auto_now_add=True)
+    abstract = models.CharField(max_length=255)
+    rose_number = models.IntegerField(default=0)
+    comment_address = models.CharField(max_length=255)
+    short_comment_list = models.ManyToManyField(ShortComment)
+
+class CreateRequest(models.Model):
+    requestor = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
+
+class CommentArea(models.Model):
+    name = models.CharField(max_length=255)
+    master = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
+    long_comment_list = models.ManyToManyField(LongComment)
+    short_comment_list = models.ManyToManyField(ShortComment)
