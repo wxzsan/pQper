9e14d87962d67de55e71eea711acd0fb12437148
Leeson <leeson.leeson@outlook.com>
2020-12-16 11:22:26 +0800

    search_index 修改 用于搜索引擎

--- commentarea/search_indexes.py
+++ commentarea/search_indexes.py
@@ -6,8 +6,8 @@ class PaperIndex(indexes.SearchIndex, indexes.Indexable):
     # 类名必须为需要检索的Model_name+Index，这里需要检索Note，所以创建NoteIndex
     # 创建一个text字段
     text = indexes.CharField(document=True, use_template=True)  
-    # 创建一个author字段
     title = indexes.CharField(model_attr='title')   
+    path = indexes.CharField(model_attr='path')
 
     # 重载get_model方法，必须要有！
     def get_model(self):          
