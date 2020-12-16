6dbf6fb7dc7f3abd38611a82cdcc7f9b294fe2bd
wxzsan <1800013102@pku.edu.cn>
2020-12-16 23:45:54 +0800

    添加了add_paper 视图

--- SearchAndResults/urls.py
+++ SearchAndResults/urls.py
@@ -1,9 +1,13 @@
 
 from django.urls import path
 from . import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = {
     path('SearchForPapersAndUsers', views.SearchForPapersAndUsers),
     path('Momments', views.GetMomments),
     path('SelfMomments', views.GetSelfMomments),
+    path('HomePage.html', TemplateView.as_view(template_name = 'HomePage.html')),
+    path('SearchResultPage.html', TemplateView.as_view(template_name = 'SearchResultPage.html')),
+    path('UserProfilePage.html', TemplateView.as_view(template_name = 'UserProfilePage.html'))
 }
