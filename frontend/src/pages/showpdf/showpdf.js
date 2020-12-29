efd9014a05f3af1d2297c6113b4211267f797859
wxzsan <1800013102@pku.edu.cn>
2020-12-29 12:01:50 +0800

    尝试显示注释

--- frontend/src/pages/showpdf/showpdf.js
+++ frontend/src/pages/showpdf/showpdf.js
@@ -8,111 +8,121 @@ import $ from 'jquery'
 
 Vue.use(ElementUI)
 Vue.prototype.$axios = axios
-  //let PDFJS = require('pdfjs-dist')
+    //let PDFJS = require('pdfjs-dist')
 
 let PDFJS = require('pdfjs-dist')
 PDFJS.workerSrc = require('pdfjs-dist/build/pdf.worker.entry')
 PDFJS.imageResourcesPath = 'http://127.0.0.1:8080/static/'
 
 var vm = new Vue({
-  el: '#app',
-  data: function () {
-    return {
-      pdf_src: '/static/A_Mathematical_Theory_of_Communication.pdf',
-      pdf_scale: 1.0, //pdf放大系数
-      pdf_pages: [],
-      pdf_div_width: ''
-    }
-  },
-  methods: {
-    handle_click: function (event) {
-      var div = document.getElementById('pdf')
-      event = event || window.event;
-      //2.获取鼠标在整个页面的位置  
-      var pagex = event.pageX || scroll().left + event.clientX;
-      var pagey = event.pageY || scroll().top + event.clientY;
-      //3.获取盒子在整个页面的位置  
-      var xx = div.offsetLeft;
-      var yy = div.offsetTop
-        //4.用鼠标的位置减去盒子的位置赋值给盒子的内容。  
-      var targetx = pagex - xx;
-      var targety = pagey - yy;
-      console.log('目标x：' + targetx)
-      console.log('目标y：' + targety)
+    el: '#app',
+    data: function() {
+        return {
+            pdf_src: '/static/A_Mathematical_Theory_of_Communication.pdf',
+            pdf_scale: 1.0, //pdf放大系数
+            pdf_pages: [],
+            pdf_div_width: ''
+        }
     },
-    _loadFile(url) { //初始化pdf
-      let loadingTask = PDFJS.getDocument(url)
-      loadingTask.promise
-        .then((pdf) => {
-          this.pdfDoc = pdf
-          this.pdf_pages = this.pdfDoc.numPages
-          this.$nextTick(() => {
-            this._renderPage(1)
-          })
-        })
-    },
-    _renderPage(num) { //渲染pdf页
-      const that = this
-      this.pdfDoc.getPage(num)
-        .then((page) => {
-          let canvas = document.getElementById('the-canvas' + num)
-          let ctx = canvas.getContext('2d')
-          let dpr = window.devicePixelRatio || 1
-          let bsr = ctx.webkitBackingStorePixelRatio ||
-            ctx.mozBackingStorePixelRatio ||
-            ctx.msBackingStorePixelRatio ||
-            ctx.oBackingStorePixelRatio ||
-            ctx.backingStorePixelRatio || 1
-          let ratio = dpr / bsr
-          let viewport = page.getViewport({ scale: this.pdf_scale })
+    methods: {
+        handle_click: function(event) {
+            var div = document.getElementById('pdf')
+            event = event || window.event;
+            //2.获取鼠标在整个页面的位置  
+            var pagex = event.pageX || scroll().left + event.clientX;
+            var pagey = event.pageY || scroll().top + event.clientY;
+            //3.获取盒子在整个页面的位置  
+            var xx = div.offsetLeft;
+            var yy = div.offsetTop
+                //4.用鼠标的位置减去盒子的位置赋值给盒子的内容。  
+            var targetx = pagex - xx;
+            var targety = pagey - yy;
+            console.log('目标x：' + targetx)
+            console.log('目标y：' + targety)
+        },
+        _loadFile: function(url) { //初始化pdf
+            let loadingTask = PDFJS.getDocument(url)
+            loadingTask.promise
+                .then((pdf) => {
+                    this.pdfDoc = pdf
+                    this.pdf_pages = this.pdfDoc.numPages
+                    this.$nextTick(() => {
+                        this._renderPage(1)
+                    })
+                })
+        },
+        _renderPage: function(num) {
+            this.pdfDoc.getPage(num)
+                .then((page) => {
+                    var scale = 1;
+                    var viewport = page.getViewport(scale);
+                    var $canvas = $('#the-canvas' + num);
+                    var canvas = $canvas.get(0);
+                    var context = canvas.getContext("2d");
+                    canvas.height = viewport.height;
+                    canvas.width = viewport.width;
 
-          canvas.width = viewport.width * ratio
-          canvas.height = viewport.height * ratio
+                    var $pdfContainer = $("#pdf");
+                    $pdfContainer.css("height", canvas.height + "px")
+                        .css("width", canvas.width + "px");
 
-          canvas.style.width = viewport.width + 'px'
+                    var renderContext = {
+                        canvasContext: context,
+                        viewport: viewport
+                    };
+                    page.render(renderContext);
+                    this.setupAnnotations(page, viewport, canvas, $('#annotation_layer' + num));
+                    if (num < this.pdf_pages) {
+                        this._renderPage(num + 1)
+                    }
+                })
+        },
 
-          that.pdf_div_width = viewport.width + 'px'
+        setupAnnotations: function(page, viewport, canvas, $annotationLayerDiv) {
+            var canvasOffset = $(canvas).offset();
+            var promise = page.getAnnotations().then(function(annotationsData) {
+                viewport = viewport.clone({
+                    dontFlip: true
+                });
 
-          canvas.style.height = viewport.height + 'px'
+                for (var i = 0; i < annotationsData.length; i++) {
+                    var data = annotationsData[i];
+                    console.log(typeof data)
+                    var annotation = PDFJS.Annotation.fromData(data);
+                    if (!annotation || !annotation.hasHtml()) {
+                        continue;
+                    }
 
-          ctx.setTransform(ratio, 0, 0, ratio, 0, 0)
-          let renderContext = {
-            canvasContext: ctx,
-            viewport: viewport
-          }
-          page.render(renderContext)
-            .then(function () {
-              // Returns a promise, on resolving it will return annotation data of page
-              return page.getAnnotations();
-            }).then(function (annotationData) {
-              // PDF canvas
-              var pdf_canvas = $("#the-canvas" + num);
+                    var element = annotation.getHtmlElement(page.commonObjs);
+                    data = annotation.getData();
+                    var rect = data.rect;
+                    var view = page.view;
+                    rect = PDFJS.Util.normalizeRect([
+                        rect[0],
+                        view[3] - rect[1] + view[1],
+                        rect[2],
+                        view[3] - rect[3] + view[1]
+                    ]);
+                    element.style.left = (canvasOffset.left + rect[0]) + 'px';
+                    element.style.top = (canvasOffset.top + rect[1]) + 'px';
+                    element.style.position = 'absolute';
 
-              // Canvas offset
-              var canvas_offset = pdf_canvas.offset();
+                    var transform = viewport.transform;
+                    var transformStr = 'matrix(' + transform.join(',') + ')';
+                    CustomStyle.setProp('transform', element, transformStr);
+                    var transformOriginStr = -rect[0] + 'px ' + -rect[1] + 'px';
+                    CustomStyle.setProp('transformOrigin', element, transformOriginStr);
 
-              // Canvas height
-              var canvas_height = pdf_canvas.get(0).height;
-
-              // Canvas width
-              var canvas_width = pdf_canvas.get(0).width;
-
-              // CSS for annotation layer
-              $("#annotation-layer" + num).css({ left: canvas_offset.left + 'px', top: canvas_offset.top + 'px', height: canvas_height + 'px', width: canvas_width + 'px' });
-
-              // Render the annotation layer
-              PDFJS.AnnotationLayer.render({
-                viewport: viewport.clone({ dontFlip: true }),
-                div: $("#annotation-layer" + num).get(0),
-                annotations: annotationData,
-                page: page
-              });
+                    if (data.subtype === 'Link' && !data.url) {
+                        // In this example,  I do not handle the `Link` annotations without url.
+                        // If you want to handle those links, see `web/page_view.js`.
+                        continue;
+                    }
+                    $annotationLayerDiv.append(element);
+                }
             });
-          if (this.pdf_pages > num) {
-            this._renderPage(num + 1)
-          }
-        })
+            return promise;
+        }
     }
-  }
 })
 vm._loadFile(vm.pdf_src)
