1e075dfb4f30d1c7229a6bd661d56e7e992b24fe
Frank Zhang <frankzhangyifeng@icloud.com>
2020-12-31 01:55:21 +0800

    分开了部分html与js文件

---
+++ frontend/src/pages/Home/HomePage.js
@@ -0,0 +1,107 @@
+/* eslint-disable */
+
+import Vue from 'vue'
+// import App from './App'
+// import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+
+Vue.use(ElementUI)
+Vue.prototype.$axios = axios
+
+var vm = new Vue({
+    el: '#app',
+    created: function() {
+        this.initDatas()
+    },
+    data: {
+        userId: 0,
+        Administrator: 0,
+        userName: "",
+        userAvatar: "",
+        updateInfoList: [],
+        searchInput: "",
+    },
+    methods: {
+        // 正则表达式匹配
+        getParams(key) {
+            var reg = new RegExp("(^|&)" + key + "=([^&]*)(&|$)")
+            var r = window.location.search.substr(1).match(reg)
+            if (r != null) {
+                return unescape(r[2])
+            }
+            return null
+        },
+        // 页面初始化，填入数据
+        initDatas() {
+
+            this.$axios.post('http://127.0.0.1:8000/user/get_user_information')
+                .then(
+                    (res) => {
+                        res = res.data
+                        if (res.code != 200) {
+                            console.log('failed to initialize')
+                            return
+                        }
+                        this.Administrator = res.data.information.privilege
+                        this.userId = res.data.information.id
+                        this.userName = res.data.information.user_name
+                        this.userAvatar = res.data.information.user_photo
+                        this.handleFresh()
+                    }
+                )
+        },
+
+        // 搜索框事件处理
+        handleSearch() {
+            if (this.searchInput.length > 0) {
+                var searchContent = btoa(encodeURI(this.searchInput))
+                window.location.href = 'http://127.0.0.1:8000/SearchAndResults/SearchResultPage.html?searchContent=' + searchContent
+            }
+        },
+
+        handleFresh() {
+            this.updateInfoList = []
+            let timestamp = Number(Date.parse(new Date()))
+            let timespan = 1000 * 3600 * 24 * 30  //设置刷新出来的天数时只需要改最后一个数
+            let data = {
+                    'userid': this.userId,
+                    'time': (timestamp - timespan)/1000,
+                }
+                let config = {
+                  headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                  },
+                }
+            this.$axios.post('http://127.0.0.1:8000/SearchAndResults/Momments', JSON.stringify(data),config)
+                .then(
+                    (res) => {
+                        res = res.data
+                        let result = res.data
+                        result.LongComments.forEach(
+                            (key) => {
+                                this.updateInfoList.push({
+                                    title: key.title,
+                                    commentid: key.id,
+                                    posterid: key.poster,
+                                    poster: key.poster_name,
+                                    posttime: key.post_time.slice(0, 19).replace('T',' '),
+                                    content: key.content,
+                                })
+                            }
+                        )
+                        if (res.code === 200) {
+                            console.log("请求成功")
+                        } else {
+                            console.log("请求失败")
+                        }
+                    }
+                )
+        },
+
+        handleDetail(id) {
+            window.location.href = "http://127.0.0.1:8000/commentarea/longComment.html?id=" + id
+        },
+    }
+})
---
+++ frontend/src/pages/searchResult/SearchResultPage.js
@@ -0,0 +1,119 @@
+/* eslint-disable */
+
+import Vue from 'vue'
+// import App from './App'
+// import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+
+Vue.use(ElementUI)
+Vue.prototype.$axios = axios
+
+var vm = new Vue({
+    el: '#app',
+    created: function(){
+        this.initDatas()
+    },
+    data: {
+        myName: "张三",
+        myAvatar: "",
+        searchMode: 0, //searchMode=1时，显示相关讨论区，为0时显示用户
+        searchUserList: [],
+
+        searchCommmentAreaList: [],
+        searchInput: "",
+    },
+    methods: {
+        // 正则表达式匹配，从上个界面的url得到searchInput
+        getParams(key){
+            var reg = new RegExp("(^|&)" + key + "=([^&]*)(&|$)")
+            var r = window.location.search.substr(1).match(reg)
+            if (r != null) {
+                return unescape(r[2])
+            }
+            return null
+        },
+        // 页面初始化，填入数据
+        initDatas(){
+            this.searchInput = atob(decodeURI(this.getParams('searchContent')));
+            this.$axios.post('http://127.0.0.1:8000/user/get_user_information')
+                .then(
+                    (res) => {
+                        res = res.data
+                        if(res.code != 200){
+                            console.log('failed to initialize')
+                            return
+                        }
+                        this.myName = res.data.information.user_name
+                        this.myAvatar = res.data.information.user_photo
+                        this.searchMode = 0
+                    }
+                )
+            if(this.searchInput.length>0)
+              this.handleSearch()
+            this.searchInput = ''
+        },
+
+        // 搜索处理
+        handleSearch(){
+            this.searchUserList = []
+            this.searchCommmentAreaLis = []
+            if(this.searchInput.length > 0){
+                  let data = {
+                    "searchString": this.searchInput,
+                  }
+                  this.$axios.get('http://127.0.0.1:8000/SearchAndResults/SearchForPapersAndUsers', {params:data})
+                    .then(
+                      (res) => {
+                        let result = res.data
+
+                        let resUser = result.data.findUsers
+                        resUser.forEach(
+                          (key)=>{
+                            this.searchUserList.push(
+                              {
+                                userId: key.id,
+                                userName: key.user_name,
+                                userAvatar: "/media/"+key.user_photo,
+                              }
+                            )
+                          }
+                        )
+                        let resCA = result.data.findCommentAreas
+                        resCA.forEach(
+                          (key)=>{
+                            this.searchCommmentAreaList.push(
+                              {
+                                id: key.id,
+                                title: key.title,
+                                address: key.path
+                              }
+                            )
+                          }
+                        )
+                        if (result.code === 200) {
+                          console.log("请求成功")
+                        } else {
+                          console.log("请求失败")
+                        }
+                      }
+                    )
+
+            }
+        },
+
+        //页面跳转处理
+        handleBack(){
+          window.location.href = "http://127.0.0.1:8000/SearchAndResults/HomePage.html"
+        },
+
+        handleUserDetail(id){
+          window.location.href = "http://127.0.0.1:8000/SearchAndResults/UserProfilePage.html?userid="+id
+        },
+
+        handleCommentAreaDetail(id){
+          window.location.href = "http://127.0.0.1:8000/commentarea/commentArea.html?id="+id
+        },
+    }
+})
